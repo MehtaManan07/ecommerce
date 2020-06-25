edc4d91557b900ad9a7bf5a4a11996c1f9d19087
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-25 13:32:06 +0530

    changed images and a bit of scss

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -4,7 +4,7 @@ const MenuItem = ({title, size, imageUrl}) => {
     return (
         <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
         <div className="content">
-          <h1 className="title"> {title} </h1>
+          <h1 className="title"> {title.toUpperCase()} </h1>
           <span className="subtitle"> SHOP NOW </span>
         </div>
       </div>
--- src/shared/DirectoryData.js
+++ src/shared/DirectoryData.js
@@ -1,35 +1,38 @@
 export const sections = [
-    {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      id: 1,
-      linkUrl: 'shop/hats'
-    },
-    {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      id: 2,
-      linkUrl: 'shop/jackets'
-    },
-    {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      id: 3,
-      linkUrl: 'shop/sneakers'
-    },
-    {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
-      id: 4,
-      linkUrl: 'shop/womens'
-    },
-    {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      size: 'large',
-      id: 5,
-      linkUrl: 'shop/mens'
-    }
-  ];
-  
+  {
+    title: "hats",
+    imageUrl:
+      "https://images.unsplash.com/photo-1514327605112-b887c0e61c0a?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    id: 1,
+    linkUrl: "shop/hats",
+  },
+  {
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    id: 2,
+    linkUrl: "shop/jackets",
+  },
+  {
+    title: "sneakers",
+    imageUrl:
+      "https://images.unsplash.com/photo-1465479423260-c4afc24172c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    id: 3,
+    linkUrl: "shop/sneakers",
+  },
+  {
+    title: "womens",
+    imageUrl:
+      "https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    size: "large",
+    id: 4,
+    linkUrl: "shop/womens",
+  },
+  {
+    title: "mens",
+    imageUrl:
+      "https://images.unsplash.com/photo-1555529669-2269763671c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    size: "large",
+    id: 5,
+    linkUrl: "shop/mens",
+  },
+];
