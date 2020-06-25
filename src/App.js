14da08956674ba738654280223806156ddad7fee
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-25 12:34:40 +0530

    basic homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Homepage from './components/homepage/Homepage';
 
 function App() {
   return (
-    <div> Hello world </div>
+    <div> 
+    <Homepage />
+     </div>
   );
 }
 
