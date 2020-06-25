654eba7ca9f4f836ed515019375f584e70bd1084
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-25 15:22:36 +0530

    SHOP_DATA

---
+++ src/pages/shop/Shop.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { SHOP_DATA } from '../../shared/ShopData'
+
+const Shop = () => {
+const [shop_data, setShop_data] = useState(SHOP_DATA)    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Shop
