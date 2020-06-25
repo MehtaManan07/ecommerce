103fb29b244b273b90f8785f1620ec4eab063e2b
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-25 13:19:46 +0530

    rendering images and a bit of scss

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
-import {sections} from '../../shared/DirectoryData'
-import MenuItem from '../menu-item/MenuItem'
-import './Directory.scss'
+import React, { useState } from "react";
+import { sections } from "../../shared/DirectoryData";
+import MenuItem from "../menu-item/MenuItem";
+import "./Directory.scss";
 
 const Directory = () => {
-    const [items, setItems] = useState(sections)
-    return (
-        <div className="directory-menu">
-            {items.map((item) => (<MenuItem key={item.title} title={item.title}  />))}
-        </div>
-    )
-}
+  const [items, setItems] = useState(sections);
+  return (
+    <div className="directory-menu">
+      {items.map((item) => (
+        <MenuItem
+          key={item.title}
+          imageUrl={item.imageUrl}
+          title={item.title}
+          size={item.size}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Directory
-
+export default Directory;
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './MenuItem.scss'
-const MenuItem = ({title}) => {
+const MenuItem = ({title, size, imageUrl}) => {
     return (
-        <div className="menu-item">
+        <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
         <div className="content">
           <h1 className="title"> {title} </h1>
           <span className="subtitle"> SHOP NOW </span>
