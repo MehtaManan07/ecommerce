9d87200dc28d7fbc74620e53cd128089934a2672
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-25 15:46:26 +0530

    rendering shop data

---
+++ src/components/preview-collection/PreviewCollection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './PreviewCollection.scss'
+const PreviewCollection = ({title, items}) => {
+    return (
+        <div className="collection-preview">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <div className="preview">
+                {items.filter((item,i) => i < 4).map((item) => (
+                    <div key={item.id}> {item.name} </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default PreviewCollection
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
 import { SHOP_DATA } from '../../shared/ShopData'
+import { useEffect } from 'react';
+import PreviewCollection from '../../components/preview-collection/PreviewCollection';
 
 const Shop = () => {
-const [shop_data, setShop_data] = useState(SHOP_DATA)    
+const [shop_data, setShop_data] = useState(SHOP_DATA)
+ useEffect(() => {
+     console.log(shop_data)
+ },[])
     return (
-        <div>
-            SHOP PAGE
+        <div className="shop-page">
+            {shop_data.map(({id, ...otherProps}) => (
+                <PreviewCollection key={id} {...otherProps} />
+            ))}
         </div>
     )
 }
