e465fe814e9366d090770facae2a4b23d92cdc0d
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-27 20:11:03 +0530

    CHECKOUT PAGE LAYOUT

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import './Checkout.scss'
-const Checkout = () => {
-    return (
-        <div>
-            khalnayak
+import React from "react";
+import "./Checkout.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/CartSelector";
+const Checkout = ({ cartItems, total }) => {
+    console.log('cartItems:', cartItems, 'total:',total)
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span> Product </span>
         </div>
-    )
-}
+        <div className="header-block">
+          <span> Description </span>
+        </div>
+        <div className="header-block">
+          <span> Quantity </span>
+        </div>
+        <div className="header-block">
+          <span> Price </span>
+        </div>
+        <div className="header-block">
+          <span> Remove </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Checkout
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(Checkout);
