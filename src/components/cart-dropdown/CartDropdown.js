d38724ca73a49733da83bc748aa664cbe3ba23ea
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-27 18:53:58 +0530

    ADDED SELECTOR FOR CART DROPDOWN

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -3,22 +3,23 @@ import "./CartDropdown.scss";
 import { connect } from "react-redux";
 import MyButton from "../myButton/MyButton";
 import CartItem from "../cart-item/CartItem";
+import { selectCartItems } from "../../redux/cart/CartSelector";
 
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-      {cartItems.map(cartItem => (
-      <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
       </div>
-        <MyButton> CHECKOUT </MyButton>
+      <MyButton> CHECKOUT </MyButton>
     </div>
   );
 };
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-  cartItems
-})
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state), // this will make sure that cartdropdown comp isn't getting rerendered whenever the state changes that is unrelated to the cart items
+});
 
 export default connect(mapStateToProps)(CartDropdown);
