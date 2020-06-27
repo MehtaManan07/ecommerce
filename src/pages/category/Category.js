6eded8434a4e625e98ce598f785919d7178f6c2d
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-28 03:40:49 +0530

    SETTING UP NESTED ROUTING

---
+++ src/pages/category/Category.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Category = ({match}) => {
+  return (
+    <div className="category">
+      <h2> Category page </h2>
+    </div>
+  );
+};
+
+export default Category;
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
+import { Route } from "react-router-dom";
+import Category from "../category/Category";
 
-
-const Shop = () => {
+const Shop = ({match}) => {
   return (
     <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={Category} />
     </div>
   );
 };
